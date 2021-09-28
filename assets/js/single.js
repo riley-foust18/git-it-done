73f71a2da735746517d24eaafac78ddc82017070
DoodleBob <riley.foust18@gmail.com>
2021-09-27 21:44:43 -0400

    added error handling and page links

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
   var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
@@ -18,9 +19,22 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      alert("There was a problem with your request!")
+      // if not succesful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
+};
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else {
+    document.location.replace("./index.html");
+  }
 };
 
 var displayIssues = function(issues) {
@@ -71,5 +85,5 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
 
